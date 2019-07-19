bd311fa391c570f482ce11d35c31d854677eaa50
Alexandra Wu <awu@clg.local>
2019-07-19 15:55:28 +0100

    might just ignore streaming media promise

--- html/shared/js/angular/qr-scanner/qr-scanner.component.js
+++ html/shared/js/angular/qr-scanner/qr-scanner.component.js
@@ -18,18 +18,22 @@
         ctrl.canvas = ctrl.canvasElement.getContext("2d");
         ctrl.loadingMessage = document.getElementById("loadingMessage");
 
-        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-                ctrl.video.srcObject = stream;
-                ctrl.video.setAttribute("playsinline", true);
-                ctrl.video.play();
-                requestAnimationFrame(ctrl.tick);
-            });
-        }
-        else {
-            console.log("this getUserMedia function is not working");
-        }
-        
+        // if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        //     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+        //         ctrl.video.srcObject = stream;
+        //         ctrl.video.setAttribute("playsinline", true);
+        //         ctrl.video.play();
+        //         requestAnimationFrame(ctrl.tick);
+        //     });
+        // }
+        // else {
+        //     console.log("this getUserMedia function is not working");
+        // }
+        ctrl.video.srcObject = stream;
+        ctrl.video.setAttribute("playsinline", true);
+        ctrl.video.play();
+        requestAnimationFrame(ctrl.tick);
+
 
         ctrl.tick = function ()
         {
