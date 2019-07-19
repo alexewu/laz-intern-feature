359cb0aa79d65544f322f2c566a38fda46fbb7e7
Alexandra Wu <awu@clg.local>
2019-07-19 20:25:04 +0100

    setup

--- html/index.php
+++ html/index.php
@@ -1,9 +1,9 @@
 <?php
 use objects\library\Router\Router;
 use objects\library\Router\Request;
-
+echo "helloooooooooooo";
 $router = new Router(new Request());
-
+echo "hellooo";
 $router->get('/', function() {
     //include 'html/index.html';
     echo "hello world";
