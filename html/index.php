492d99fc1d25e4819b4280076ca626853519edcd
Alexandra Wu <alexewu@umich.edu>
2019-07-19 15:35:26 -0400

    Question mark.

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <?php
-use html\objects\library\Router\Router;
-use html\objects\library\Router\Request;
-echo "helloooooooooooo";
+use objects\library\Router\Router;
+use objects\library\Router\Request;
+echo "helloooooooooooo?";
 $router = new Router(new Request());
 echo "hellooo";
 $router->get('/', function() {
