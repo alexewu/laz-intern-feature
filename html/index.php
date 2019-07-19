9b10b20b67a56affff837a6b30ca5ed8ac515c95
Alexandra Wu <awu@clg.local>
2019-07-19 20:17:20 +0100

    setup

--- html/index.php
+++ html/index.php
@@ -1,14 +1,13 @@
 <?php
-//use objects\library\Router\Router;
-//use objects\library\Router\Request;
-//
-//$router = new Router(new Request());
-//
-//$router->get('/', function() {
-//    include 'html/index.html';
-//});
-//
-//$router->get('/featureDescription', function() {
-//    return "Qr Student Login :)";
-//});
-echo "hello world";
+use objects\library\Router\Router;
+use objects\library\Router\Request;
+
+$router = new Router(new Request());
+
+$router->get('/', function() {
+    include 'html/index.html';
+});
+
+$router->get('/featureDescription', function() {
+    return "Qr Student Login :)";
+});
