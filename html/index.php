c1a5cc52aaa0347ae2fdabb8cb948dee48f40e0c
Alexandra Wu <awu@clg.local>
2019-07-22 21:45:08 +0100

    testing webhook

--- html/index.php
+++ html/index.php
@@ -1,18 +1,19 @@
 <?php
-require_once './objects/library/Router/Request.php';
-require_once './objects/library/Router/Router.php';
-
-use objects\library\Router\Request;
-use objects\library\Router\Router;
-
-$request = new Request();
-$router = new Router($request);
-
-$router->get('/', function() {
-    //include 'html/index.html';
-    echo "hello world";
-});
-
-$router->get('/featureDescription', function() {
-    return "Qr Student Login :)";
-});
+echo "hello worrrrldlddldld";
+//require_once './objects/library/Router/Request.php';
+//require_once './objects/library/Router/Router.php';
+//
+//use objects\library\Router\Request;
+//use objects\library\Router\Router;
+//
+//$request = new Request();
+//$router = new Router($request);
+//
+//$router->get('/', function() {
+//    //include 'html/index.html';
+//    echo "hello world";
+//});
+//
+//$router->get('/featureDescription', function() {
+//    return "Qr Student Login :)";
+//});
