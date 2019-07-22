3dbd679f8eb129d41ece143d99dbaac4b5c7d67d
Alexandra Wu <awu@clg.local>
2019-07-22 14:53:01 +0100

    added namespace names

--- html/index.php
+++ html/index.php
@@ -2,6 +2,9 @@
 require_once './objects/library/Router/Request.php';
 require_once './objects/library/Router/Router.php';
 
+use objects\library\Router\Request;
+use objects\library\Router\Router;
+
 $request = new Request();
 $router = new Router($request);
 
