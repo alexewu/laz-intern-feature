e66d1001c84350f1ac57a6f98c37df09803fef33
Alexandra Wu <awu@clg.local>
2019-07-23 18:43:45 +0100

    just creating request obj

--- html/index.php
+++ html/index.php
@@ -1,18 +1,18 @@
 <?php
 require_once './objects/library/Router/Request.php';
 require_once './objects/library/Router/Router.php';
-
-use objects\library\Router\Request;
-use objects\library\Router\Router;
+//
+//use objects\library\Router\Request;
+//use objects\library\Router\Router;
 
 $request = new Request();
 $router = new Router($request);
 
-$router->get('/', function() {
-    //include 'html/index.html';
-    echo "hello world";
-});
-
-$router->get('/featureDescription', function() {
-    return "Qr Student Login :)";
-});
+//$router->get('/', function() {
+//    //include 'html/index.html';
+//    echo "hello world";
+//});
+//
+//$router->get('/featureDescription', function() {
+//    return "Qr Student Login :)";
+//});
