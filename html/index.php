d7ef81d7bb50a34bcf55cbab2b5588762aae850f
Alexandra Wu <awu@clg.local>
2019-07-23 18:50:15 +0100

    just creating request obj

--- html/index.php
+++ html/index.php
@@ -1,12 +1,15 @@
 <?php
-require_once './objects/library/Router/Request.php';
-require_once './objects/library/Router/Router.php';
-//
-//use objects\library\Router\Request;
-//use objects\library\Router\Router;
+require_once 'objects/library/Router/Request.php';
+require_once 'objects/library/Router/Router.php';
+
+use objects\library\Router\Request;
+use objects\library\Router\Router;
 
 $request = new Request();
+echo "jfkasd";
 $router = new Router($request);
+echo "jhaskldf";
+
 
 //$router->get('/', function() {
 //    //include 'html/index.html';
