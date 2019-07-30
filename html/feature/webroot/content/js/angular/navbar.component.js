01a1b6d14a92782a98cf5c3f3a3b4f9813a2fc08
Alexandra Wu <alexewu@umich.edu>
2019-07-30 20:58:35 +0100

    created navbar component

---
+++ html/feature/webroot/content/js/angular/navbar.component.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var app = angular.module("feature");
+
+    app.component("navbar", {
+        templateUrl: 'feature/webroot/content/js/angular/navbar.html',
+        controller: 'navbarController',
+    });
+
+    app.controller('navbarController', navbarController);
+
+    function navbarController() {
+        var ctrl = this;
+
+
+    }
+})();
