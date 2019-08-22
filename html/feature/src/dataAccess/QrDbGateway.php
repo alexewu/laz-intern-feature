311b82ce3eb337f07ffbe640b840aa885197366d
Alexandra Wu <alexewu@umich.edu>
2019-08-22 16:16:58 +0100

    uncommented all queries cuz database works :)

--- html/feature/src/dataAccess/QrDbGateway.php
+++ html/feature/src/dataAccess/QrDbGateway.php
@@ -14,14 +14,6 @@ class QrDbGateway {
     private $qrDm;
 
     public function __construct(int $shardId) {
-        // $this->shardId = $shardId;
-        // $this->student = [ //mock student data
-        //     "student_id" => 12345,
-        //     'qr_password' => '195d628dbdb59083657dhbthgu73892784nhxjud8394857b2',
-        //     'student_first_name' => 'Alexandra',
-        //     'student_last_name' => 'Wu',
-        //     'screen_name' => 'alexewu'
-        // ];
         $this->qrDm = new DataManager();
     }
 
@@ -38,76 +30,67 @@ class QrDbGateway {
     }
 
     public function isStudentInQrTable(int $studentId): bool {
-//        $sql = "SELECT 1
-//                FROM qr_student_login
-//                WHERE student_id = $studentId";
-//
-//        $this->qrDm->query($sql);
-//        return !!$this->qrDm->fetch();
-        return true;
+        $sql = "SELECT 1
+                FROM qr_student_login
+                WHERE student_id = $studentId";
+
+        $this->qrDm->query($sql);
+        return !!$this->qrDm->fetch();
     }
 
     public function createFirstQRPassword(int $studentId, string $newPassword): void {
-//        $sql = "INSERT INTO qr_student_login(student_id, qr_password)
-//                VALUES ($studentId, '$newPassword')";
-//
-//        $this->qrDm->query($sql);
+        $sql = "INSERT INTO qr_student_login(student_id, qr_password)
+                VALUES ($studentId, '$newPassword')";
+
+        $this->qrDm->query($sql);
     }
 
     public function updateQRPassword(int $studentId, string $newPassword): void {
-//        $sql = "UPDATE qr_student_login
-//                SET qr_password = '$newPassword'
-//                WHERE student_id = $studentId";
-//
-//        $this->qrDm->query($sql);
-        $this->student['qr_password'] = $newPassword;
+        $sql = "UPDATE qr_student_login
+                SET qr_password = '$newPassword'
+                WHERE student_id = $studentId";
+
+        $this->qrDm->query($sql);
     }
 
     public function getStudentIdFromQrPassword(string $qrPassword): ?int {
-//        if (strlen($qrPassword) != QrDbGateway::MAX_QR_PASSWORD_LENGTH) {
-//            return null;
-//        }
+        if (strlen($qrPassword) != QrDbGateway::MAX_QR_PASSWORD_LENGTH) {
+            return null;
+        }
 
-//        $sql = "SELECT student_id
-//                FROM qr_student_login
-//                WHERE qr_password = '$qrPassword'";
-//
-//        $this->qrDm->query($sql);
-//        $row = $this->qrDm->fetch();
-//
-//        return (int)$row["student_id"];
+        $sql = "SELECT student_id
+                FROM qr_student_login
+                WHERE qr_password = '$qrPassword'";
 
-        return 12345;
+        $this->qrDm->query($sql);
+        $row = $this->qrDm->fetch();
+
+        return (int)$row["student_id"];
     }
 
     public function getMemberIdFromStudentId(int $studentId): ?int {
-//        $sql = "SELECT homeroom_member_id
-//                FROM `student`
-//                WHERE student_id = $studentId";
-//
-//        $this->qrDm->query($sql);
-//        $row = $this->qrDm->fetch();
-//
-//        return (int)$row["homeroom_member_id"];
-        return 54321;
+        $sql = "SELECT homeroom_member_id
+                FROM `student`
+                WHERE student_id = $studentId";
+
+        $this->qrDm->query($sql);
+        $row = $this->qrDm->fetch();
+
+        return (int)$row["homeroom_member_id"];
     }
 
     public function getQrInfo(int $memberId): array {
-//        $sql = "SELECT qr_student_login.student_id,
-//                        qr_student_login.qr_password,
-//                        student.student_first_name,
-//                        student.student_last_name,
-//                        student.screen_name
-//                FROM qr_student_login JOIN student
-//                ON qr_student_login.student_id = student.student_id
-//                WHERE homeroom_member_id=$memberId";
-//
-//        $this->qrDm->query($sql);
-//        return $this->qrDm->fetchAll();
-        return $this->student;
-    }
+        $sql = "SELECT qr_student_login.student_id,
+                        qr_student_login.qr_password,
+                        student.student_first_name,
+                        student.student_last_name,
+                        student.screen_name
+                FROM qr_student_login JOIN student
+                ON qr_student_login.student_id = student.student_id
+                WHERE homeroom_member_id=$memberId";
 
-    public function login() {
-        return "You have logged in!";
+        $this->qrDm->query($sql);
+        return $this->qrDm->fetchAll();
     }
+    
 }
